a6f9730cd081d4383d0006cb9752e74e3013d51a
pirasalbe <alberto98.piras@gmail.com>
2017-02-25 10:56:47 +0100

    [TODO] php with 4 query

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "artisti";
+	$dbname = "orchestrapiras";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
